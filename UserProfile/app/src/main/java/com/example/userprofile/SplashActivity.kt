efe90198d505a0fd4bf84c1859afb75c1b04daaa
IrisSaskia <iris.oostra@hva.nl>
2020-03-21 19:39:43 +0100

    2.1 Splash Screen - Add a new style

--- UserProfile/app/src/main/java/com/example/userprofile/SplashActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.example.userprofile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
